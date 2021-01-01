4a964579e83311d62c7f2934ad6683f3b10d6dc1
Adminluck <38484134+Adminluck@users.noreply.github.com>
2021-01-01 14:52:25 +0800

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -9,11 +9,11 @@ sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
 wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
 sudo dpkg --install nordvpn-release_1.0.0_all.deb
-sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
